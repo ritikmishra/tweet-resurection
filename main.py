b7c9f982a629bc0147cecd6d50c59934c5d7de86
Ritik Mishra <ritik.mishra314@gmail.com>
2015-02-14 22:44:37 -0600

    Added 99% functionality

--- main.py
+++ main.py
@@ -1,20 +1,65 @@
 import tweepy
 import time
-#Authenticates twitter API
-auth = tweepy.OAuthHandler('V4kncK07KHZ2V9RRePa7myQ7l', '2dxr53rpTtpCNM1yUqE0ofQxg0wZC2lZy66FHow89MERTaQjTO')
-auth.set_access_token('838703438-7fD9TxqICgCWsZi35MGjve60J15u9ViTWIdIbEZe', 'qwLmo3FaQU1JN0sBqqqE1pNsylNYPaTs5weXzU6PiRj4U')
-
-api = tweepy.API(auth)
-user = api.get_user("ritmish")
-print user.screen_name
-followers = []
-for friend in user.followers():
-	followers.append(friend.screen_name)
-	
+import random
+import getpass
+from sys import exit
 ids = []
-for page in tweepy.Cursor(api.followers_ids, screen_name="xkcd").pages():
-    ids.extend(page)
-    time.sleep(1)
-    
-screen_names = [user.screen_name for user in api.lookup_users(user_ids=ids)]
-print screen_names
+def get_statuses():
+	global ids, api, creepee, last_tweet
+	previous_ids = []
+	ids = []
+	
+	for status in api.user_timeline(creepee,count=200, page=2):
+		ids.append(status.id)
+		print status.author
+		print " said \"" + status.text + "\""
+		last_tweet = ids[len(ids)-1]
+	
+	api.retweet(last_tweet)
+	
+def main(app_set):
+	global ids, api, creepee, last_tweet
+	try:
+		consumer_key=["V4kncK07KHZ2V9RRePa7myQ7l", "Du6zndJP8C5ZqIozpPyIWMMIy"]
+		consumer_secret=['HIDDEN']
+		access_token=["838703438-7fD9TxqICgCWsZi35MGjve60J15u9ViTWIdIbEZe", "838703438-4IZl6z7SAj7DE2NcSD8nyNgsKz4cexpMc0I8djPK"]
+		access_secret=['HIDDEN']
+		app_set = app_set
+		#Authenticates twitter API
+		auth = tweepy.OAuthHandler(consumer_key[app_set], consumer_secret[app_set])
+		#Joseph said he hates OAuth
+		auth.set_access_token(access_token[app_set], access_secret[app_set])
+		api = tweepy.API(auth)
+		username = raw_input(str("Twitter Handle: "))
+		password = getpass.getpass("Twitter Password: ")
+		
+		if username[0] == '@':
+			username = username[1:]
+		user = api.get_user(username)
+		
+		print "Logged in as " + user.screen_name
+		
+		followers = []
+		ids = []
+		
+		for friend in user.followers():
+			if not friend.protected:
+				followers.append(friend.screen_name)
+		
+		for x in followers:
+			print x
+		
+		follower_numbers = range(len(followers))
+		creepee = str(raw_input("Above are your Creep-able followers. Enter a Twitter handle to resurect an old tweet."))
+		if creepee[0] == '@':
+			creepee = crepee[1:]
+		get_statuses()
+	except tweepy.error.TweepError:
+		last_tweet = ids[len(ids)-1]
+		api.retweet(last_tweet)
+		exit("Crashed with an error.")
+		
+	finally:
+		print("Exiting...")
+main(0)
+		
