ba6bec5415e2e1ddfe59fecd57964be5cd72b77f
Ritik Mishra <ritik.mishra314@gmail.com>
2015-02-15 06:41:49 -0600

    No longer retweets. Outputs ~200 tweets to a text file.

--- main.py
+++ main.py
@@ -1,43 +1,56 @@
 import tweepy
 import time
 import random
-import getpass
+import guidata
+import guidata.dataset.datatypes as dt
+import guidata.dataset.dataitems as di
+
+    
 from sys import exit
 ids = []
+last_tweet = 0
 def get_statuses():
-	global ids, api, creepee, last_tweet
+	global ids, api, creepee, last_tweet, output
 	previous_ids = []
 	ids = []
 	
+	
 	for status in api.user_timeline(creepee,count=200, page=2):
 		ids.append(status.id)
-		print status.author
-		print " said \"" + status.text + "\""
 		last_tweet = ids[len(ids)-1]
+		appended_content = unicode(status.author.screen_name + " said \"" + status.text + "\" \n")
+		output.write(appended_content.encode('ascii', 'ignore'))
+		
+
 	
-	api.retweet(last_tweet)
 	
+class Processing(dt.DataSet):
+    """Example"""
+    
+    b = di.StringItem("Username", default="App will return most recent 200 tweets from this person")
+    c = di.ButtonItem("Test", get_statuses)
+
+
 def main(app_set):
-	global ids, api, creepee, last_tweet
+	global ids, api, creepee, last_tweet, output
 	try:
-		consumer_key=["V4kncK07KHZ2V9RRePa7myQ7l", "Du6zndJP8C5ZqIozpPyIWMMIy"]
-		consumer_secret=['HIDDEN']
-		access_token=["838703438-7fD9TxqICgCWsZi35MGjve60J15u9ViTWIdIbEZe", "838703438-4IZl6z7SAj7DE2NcSD8nyNgsKz4cexpMc0I8djPK"]
-		access_secret=['HIDDEN']
+		consumer_key=["HIDDEN"]
+		consumer_secret=["Hidden"]
+		access_token=["HIDDEN"]
+		access_secret=["Hiddens"]
 		app_set = app_set
 		#Authenticates twitter API
 		auth = tweepy.OAuthHandler(consumer_key[app_set], consumer_secret[app_set])
 		#Joseph said he hates OAuth
 		auth.set_access_token(access_token[app_set], access_secret[app_set])
 		api = tweepy.API(auth)
-		username = raw_input(str("Twitter Handle: "))
-		password = getpass.getpass("Twitter Password: ")
+		username = raw_input(str("Enter a Twitter handle to get its followers: "))
 		
 		if username[0] == '@':
 			username = username[1:]
 		user = api.get_user(username)
 		
-		print "Logged in as " + user.screen_name
+		
 		
 		followers = []
 		ids = []
@@ -52,13 +65,15 @@ def main(app_set):
 		follower_numbers = range(len(followers))
 		creepee = str(raw_input("Above are your Creep-able followers. Enter a Twitter handle to resurect an old tweet."))
 		if creepee[0] == '@':
-			creepee = crepee[1:]
+			creepee = creepee[1:]
+		output = open(creepee + '.txt', 'a')
 		get_statuses()
 	except tweepy.error.TweepError:
 		last_tweet = ids[len(ids)-1]
 		api.retweet(last_tweet)
 		exit("Crashed with an error.")
-		
+	except IndexError:
+		print("This user doesn't have 200 tweets, the minimum required to automatically creep.")		
 	finally:
 		print("Exiting...")
 main(0)
